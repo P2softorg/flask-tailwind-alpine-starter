103124599bd1068227b86adf662a0dd2016e6b32
Phil <plrtn@protonmail.com>
2021-09-04 10:52:00 -0400

    setups for postcss to tailwind build process

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+	'./static/src/**/*.html',
+	'./static/src/**/*.js'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
