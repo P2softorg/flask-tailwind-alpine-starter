1d9e97944ec70ca8203a535772f2b4f19b6e6235
Phil <plrtn@protonmail.com>
2021-09-04 11:00:31 -0400

    change app.py for paths tocss files

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 from flask import Flask
+from flask_assets import Bundle, Environment
+
+# Config
+
 app = Flask(__name__)
+assets = Environment(app)
+
+# By default, flask looks for files inside the ./static/ folder
+# So the path relative to project is "./static/src/main.css"
+css = Bundle("src/main.css", output="dist/main.css", filters="postcss")
+assets.register("css", css)
+css.build()
 
 @app.route("/")
 def home():
